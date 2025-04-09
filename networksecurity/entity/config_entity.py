d3c81d2b2d34e14a53585a58eb86db4432bc6713
Ashish709 <93144415+Ashish709@users.noreply.github.com>
2025-04-09 20:29:28 +0530

    Data Ingestion Pipeline

--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -12,7 +12,8 @@ class TrainingPipelineConfig:
         timestamp = timestamp.strftime("%m_%d_%Y_%H_%M_%S")
         self.pipeline_name = training_pipeline.PIPELINE_NAME
         self.artifact_name = training_pipeline.ARTIFACT_DIR
-        self.artifact_dir = os.path.join(self.artifact_dir,timestamp)
+        self.artifact_dir = os.path.join(self.artifact_name,timestamp)
+        self.model_dir=os.path.join("final_model")
         self.timestamp:str = timestamp
         
 
@@ -36,6 +37,6 @@ class DataIngestionConfig:
         self.collection_name: str = training_pipeline.DATA_INGESTION_COLLECTION_NAME
         
         self.database_name:str = training_pipeline.DATA_INGESTION_DATABASE_NAME
-        pass
+        
         
     
