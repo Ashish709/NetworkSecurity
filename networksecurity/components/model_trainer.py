84fd2047432b7801b6c6ee3a9f016068aab6ca43
Ashish709 <93144415+Ashish709@users.noreply.github.com>
2025-04-14 00:13:50 +0530

    Model Trainer with mlflow

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -34,6 +34,18 @@ class ModelTrainder:
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
+    def track_mlflow(self,best_model, classification_metric):
+        with mlflow.start_run():
+            f1_score = classification_metric.f1_score
+            precision_score = classification_metric.precision_score
+            recall_score = classification_metric.recall_score
+            
+            mlflow.log_metric("f1_score",f1_score)
+            mlflow.log_metric("precision_score",precision_score)
+            mlflow.log_metric("recall_score",recall_score)
+            
+            mlflow.sklearn.log_model(best_model,"model")
+        
     
     def train_model(self,x_train,y_train,x_test,y_test):
         models = {
@@ -47,6 +59,7 @@ class ModelTrainder:
             "Decision Tree": {
                 'criterion':['gini', 'entropy', 'log_loss'],
                 # 'splitter':['best','random'],
+                
                 # 'max_features':['sqrt','log2'],
             },
             "Random Forest":{
@@ -88,13 +101,13 @@ class ModelTrainder:
         classification_train_metric=get_classification_score(y_true=y_train,y_pred=y_train_pred)
         
         ## Track the experiements with mlflow
-        # self.track_mlflow(best_model,classification_train_metric)
+        self.track_mlflow(best_model,classification_train_metric)
 
 
         y_test_pred=best_model.predict(x_test)
         classification_test_metric=get_classification_score(y_true=y_test,y_pred=y_test_pred)
 
-        # self.track_mlflow(best_model,classification_test_metric)
+        self.track_mlflow(best_model,classification_test_metric)
 
         preprocessor = load_object(file_path=self.data_transformation_artifact.transformed_object_file_path)
             
